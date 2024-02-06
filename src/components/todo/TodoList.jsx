9138e047b6982bf413f5105741fe9832aacf13f6
eonhyo <syuku94069@gmail.com>
2024-02-06 15:17:38 +0900

    3번까지 완료

---
+++ src/components/todo/TodoList.jsx
@@ -0,0 +1,29 @@
+import TodoItem from "./TodoItem";
+
+const TodoList = ({ headTitle, todos, onDeleteTodoItem, onToggleTodoItem }) => {
+  return (
+    <section>
+      <h2>{headTitle}</h2>
+      <ul>
+        {
+          todos.map(todo => (
+            <TodoItem
+              key = {todo.id}
+              todo = {todo}
+              onDeleteTodoItem={()=>onDeleteTodoItem(todo.id)}
+              onToggleTodoItem={()=>onToggleTodoItem(todo.id)}
+            />
+          ))
+
+
+          // SECTION: 1번 문제
+          // TODO: 투두 리스트 렌더링
+          // NOTE: map 메서드를 사용하여 `TodoItem` 컴포넌트 렌더링
+          // HINT: `TodoItem` 컴포넌트에는 `todo`, `onDeleteTodoItem`, `onToggleTodoItem` props를 전달
+        }
+      </ul>
+    </section>
+  );
+};
+
+export default TodoList;
